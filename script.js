2b821746a1e9c7b6c3a2aa494277f9905a9881e8
sarasolkarrr <sara.ssolkar@gmail.com>
2026-03-20 09:29:41 +0530

    initial upload

---
+++ script.js
@@ -0,0 +1,60 @@
+const raincontainer = document.getElementById("raincontainer");
+const numberOfRaindrops = 200;
+function crd() {
+    const rd = document.createElement("div");
+    rd.className = "raindrop";
+
+    const left = Math.random() * 100;
+    const width = Math.random() * 2 + 1;
+    const height = Math.random() * 50 + 70;
+    const duration = 2;
+    const delay = Math.random();
+    rd.style.left = left + "%";
+    rd.style.width = width + "px";
+    rd.style.height = height + "px";
+    rd.style.animationDuration = duration + "s";
+    rd.style.animationDelay = delay + "s";
+    return rd;
+}
+function rain() {
+    raincontainer.innerHTML = "";
+
+    for (let i = 0; i < numberOfRaindrops; i++) {
+        raincontainer.appendChild(crd());
+    }
+    setTimeout(() => {
+        raincontainer.innerHTML = "";
+        document.body.style.backgroundImage = "url(jimc.png)";
+            document.body.innerHTML += `
+            <div style="
+                position: fixed;
+                top: 0;
+                left: 0;
+                width: 100%;
+                height: 100%;
+                background: rgba(228, 236, 230, 0.7);
+                padding: 120px 30px 40px;
+                box-sizing: border-box;
+                overflow-y: auto;
+                border-radius: 10px;
+                text-align: center;
+            ">
+                <h2 style="text-align: center">Forest Fire Impact</h2>
+                <p>
+                Forest fires in Jim Corbett National Park are a common problem, especially during the dry summer months when leaves and grass become highly flammable.
+                Many of these fires are caused by human activities like carelessness, burning waste, or illegal entry into forest areas, though natural causes can also occur. 
+                One exmaple of these forest fires is the 2016 Uttarakhand forest fire that spread to approximately 4,538 hectares (11,210 acres) of forest land and resulted in nine fatalities
+                To manage forest fires,the forest officials use methods like controlled burning in winter to reduce dry vegetation and prevent larger fires. 
+                Rain being a natural suppresant of these fires can only be available seasonally so in serious situations, help from the Indian Air Force is taken, where helicopters are used to drop water using Bambi buckets.
+                These efforts are important to protect wildlife, forests, and the overall ecosystem of the park.
+                </p>
+                <div>
+                <img src="forestfirer.jpeg">
+                <p style="text-align:center">Helicopters being used to supress the forest fire</p>
+                </div>
+                
+                <h2>
+            </div>
+        `;
+    }, 1000);
+}
